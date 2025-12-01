0a4dd8599708f4ecbd84b3da68c3b20ee001858c
Altanbagana <n.altanbagana2003@gmail.com>
2025-12-01 20:40:15 +0800

    image fixed

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["localhost"],
+    domains: ["localhost", "api.cubingmongolia.mn"],
     remotePatterns: [
       {
         protocol: "http",
